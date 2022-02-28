fef98a242583780121ae14a81c29f7ea3be9f040
ddhillon-fn <dilsher.dhillon@freenome.com>
2022-02-28 04:00:42 +0000

    WIP replicate analysis

--- notebooks/process-pseudo-guardbanding/render.r
+++ notebooks/process-pseudo-guardbanding/render.r
@@ -1,3 +1,10 @@
+## flexmap distributions 
 rmarkdown::render(here::here("notebooks", "process-pseudo-guardbanding", "02.Rmd"),
                   output_file = here::here("notebooks", "process-pseudo-guardbanding", "outputs", 
                                            "distribution_flexmaps_hamiltons.html"))
+
+
+## failure modes 
+rmarkdown::render(here::here("notebooks", "process-pseudo-guardbanding", "failure_modes.Rmd"),
+                  output_file = here::here("notebooks", "process-pseudo-guardbanding", "outputs", 
+                                           "failure_modes_standard_curve.html"))
