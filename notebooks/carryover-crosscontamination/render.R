f3579a4deb73d98bda6d12f80f751f7a4a375925
ddhillon-fn <dilsher.dhillon@freenome.com>
2022-03-15 14:29:04 +0000

    first iteration of carryover

---
+++ notebooks/carryover-crosscontamination/render.R
@@ -0,0 +1,5 @@
+## carryover cross contamination study 
+
+rmarkdown::render(here::here("notebooks", "carryover-crosscontamination", "dev-study-01.rmd"),
+                  output_file = here::here("notebooks", "carryover-crosscontamination", "outputs", 
+                                           "dev-study-01.html"))
