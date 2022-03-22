6d2dbf6ca62cc01e7a65dd454028676b4c5d7d13
ddhillon-fn <dilsher.dhillon@freenome.com>
2022-03-22 20:56:25 +0000

    first iteration of xponent vs scipy implementation using all 4 replicates

---
+++ notebooks/xMAP-implementation-verify/render.r
@@ -0,0 +1,4 @@
+## xponent vs scipy 
+rmarkdown::render(here::here("notebooks", "xMAP-implementation-verify", "xponent-vs-scipy.Rmd"),
+                  output_file = here::here("notebooks", "xMAP-implementation-verify", "outputs", 
+                                           "xponent-vs-scipy.html"))
